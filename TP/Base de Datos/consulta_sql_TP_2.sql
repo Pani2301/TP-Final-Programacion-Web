3b73704d28aa3cb91183e1cfdfa3a7a5113a6558
Joaquin Panichelli <jpanichelli@udesa.edu.ar>
2025-11-10 08:31:20 -0300

    agregue la base de datos y consultas de sql

--- TP/Base de Datos/consulta_sql_TP_2.sql
+++ TP/Base de Datos/consulta_sql_TP_2.sql
@@ -1,37 +1,38 @@
 SHOW TABLES;
 
--- 1
+-- 1 (Muestra todos los usuarios)
 SELECT * FROM `usuario`;
 
--- 2
+-- 2 (Limita a mostrar 3 Peliculas)
 SELECT * FROM `pelicula` LIMIT 3;
 
--- 3
+-- 3 (Muestra los id de compra de la segunda funcion)
 SELECT * FROM `compra` WHERE `id_funcion`=1;
 
--- 4
+-- 4 (Muestra los usuarios que tenga Fer en el apellido y que pueda tener mas cosas)
 SELECT * FROM `usuario` WHERE `apellido` LIKE '%Fer%';
 
--- 5
+-- 5 (Seleccionar los usuarios con id 1 y 3 y entre 1 y 5)
 SELECT * FROM `usuario` WHERE id IN (1, 3);
-
 SELECT * FROM `usuario` WHERE id BETWEEN 1 AND 3;
 
 
--- 6
+-- 6 (Muestra los nombres de los usuarios con id 1 y 3 y entre 1 y 5)
 SELECT `nombre` from `usuario` WHERE id IN (1, 5);
 SELECT `nombre` from `usuario` WHERE id BETWEEN 1 and 5;
 
--- 7 
+
+-- 7 (Junta las Tablas y selecciona la segunda compra)
 SELECT * FROM `compra`
 INNER JOIN `entrada` ON compra.id = entrada.id_compra
 WHERE compra.id = 2;
 
--- 8
-SELECT butacas.codigo_butaca as numero_de_asiento FROM `butacas` INNER JOIN `entrada` 
+-- 8 (Ver las butacas seleccionadas en la sala 1)
+SELECT id_sala, butacas.codigo_butaca as numero_de_asiento FROM `butacas` INNER JOIN `entrada` 
 ON entrada.id_butaca = butacas.id
+where id_sala=1;
 
--- 9
+-- 9 (Cuanto gasto cada usuario)
 SELECT usuario.nombre, 
 usuario.apellido, 
 SUM(compra.precio) as Total_Gasto 
@@ -40,25 +41,27 @@ INNER JOIN compra ON entrada.id_compra = compra.id
 INNER JOIN usuario ON compra.id_usuario = usuario.id
 GROUP BY usuario.id
 ORDER BY Total_Gasto DESC;
-------------------------------------------------
-SELECT usuario.nombre, 
-usuario.apellido, 
-SUM(compra.precio) as Total_Gasto 
-FROM entrada
-INNER JOIN compra ON entrada.id_compra = compra.id
-INNER JOIN usuario ON compra.id_usuario = usuario.id
-GROUP BY usuario.id
-HAVING Total_Gasto > 3000
-ORDER BY Total_Gasto DESC;
-------------------------------------------------
 
 
--- 10
+
+-- 10 (Cantidad de entradas vendidas de cada Pelicula)
 SELECT pelicula.titulo, COUNT(entrada.id) as Cantidad_Entradas_Vendidas FROM `pelicula`
 INNER JOIN funciones ON pelicula.id = funciones.id_pelicula
 INNER JOIN compra ON funciones.id = compra.id_funcion
 INNER JOIN entrada ON compra.id = entrada.id_compra
 GROUP BY pelicula.titulo;
 
+-- 11 (Cuanto recaudo cada Pelicula)
+SELECT pelicula.titulo, SUM(compra.precio) AS Recaudacion_Total
+FROM pelicula
+INNER JOIN funciones ON pelicula.id = funciones.id_pelicula
+INNER JOIN compra ON funciones.id = compra.id_funcion
+INNER JOIN entrada ON compra.id = entrada.id_compra
+GROUP BY pelicula.titulo
+ORDER BY Recaudacion_Total DESC;
 
-
+-- 12 (Usuarios que no compraron nada)
+SELECT usuario.id, usuario.nombre, usuario.apellido
+FROM usuario
+LEFT JOIN compra ON usuario.id = compra.id_usuario
+WHERE compra.id IS NULL;
--- TP/Base de Datos/consultas_sql_TP.sql
+++ TP/Base de Datos/consultas_sql_TP.sql
@@ -1,23 +1,22 @@
 SHOW DATABASES;
-
 use `base de datos ruba`;
-
 SHOW TABLES;
-
 
 -- USUARIOS
 DESCRIBE `usuario`;
-
 SELECT * FROM `usuario`;
 
-INSERT INTO `usuario`(id, `nombre`, `apellido`, `fecha_de_nacimiento`, `email`, `contrasena`) 
+
+INSERT INTO `usuario`
+(id, `nombre`, `apellido`, `fecha_de_nacimiento`, `email`, `contrasena`) 
 VALUES
-(NULL, 'Joaquin', 'Panichelli', '2006-06-30', 'joaquin@gmail.com', 'Joaquin1234')
+(NULL, 'Joaquin', 'Panichelli', '2006-06-30', 'joaquin@gmail.com', 'Joaquin1234');
+
 
 --PELICULAS
 DESCRIBE `pelicula`;
-
 SELECT * FROM `pelicula`;
+
 
 INSERT INTO `pelicula`
 (id, `titulo`, `origen`, `genero`, `sinopsis`, `trailer`, `duracion`, `estado`)
@@ -31,13 +30,14 @@ VALUES
 120, 
 'En cartelera');
 
+
 DELETE FROM `pelicula` WHERE `id`=9;
 
 
 -- SALAS
 DESCRIBE `salas`;
-
 SELECT * FROM `salas`;
+
 
 INSERT INTO `salas`
 (id, `capacidad`)
@@ -47,8 +47,8 @@ VALUES
 
 -- FUNCIONES
 DESCRIBE `funciones`;
-
 SELECT * FROM `funciones`;
+
 
 INSERT INTO `funciones`
 (id, `id_pelicula`, `id_sala`, `horario`)
@@ -56,11 +56,10 @@ VALUES
 (NULL, 1, 1, '2024-07-01 20:00:00');
 
 
-
 -- COMPRA
 DESCRIBE `compra`;
-
 SELECT * FROM `compra`;
+
 
 INSERT INTO `compra`
 (id, `id_funcion`, `id_usuario`, `precio`, `fecha_compra`)
@@ -73,8 +72,8 @@ UPDATE `compra` SET `precio`=3000 WHERE `id`=1;
 
 -- BUTACAS
 DESCRIBE `butacas`;
-
 SELECT * FROM `butacas`;
+
 
 INSERT INTO `butacas`
 (id, `id_sala`, `codigo_butaca`, `estado`, `precio`)
@@ -91,8 +90,8 @@ VALUES
 
 -- ENTRADA
 DESCRIBE `entrada`;
-
 SELECT * FROM `entrada`;
+
 
 INSERT INTO `entrada`
 (id, `id_compra`, `id_butaca`)
@@ -100,18 +99,17 @@ VALUES
 (NULL, 6, 163),
 (NULL, 6, 164);
 
+
 DELETE FROM `entrada` WHERE `id`=11;
 
 
-SELECT * FROM `entrada` INNER JOIN 
-
 -- CONSULTAS
-
 DESCRIBE `consultas`;
-
 SELECT * FROM `consultas`;
+
 
 INSERT INTO `consultas`
 (id, `id_usuario`, `mensaje`, `hora`)
 VALUES
 (NULL, 2, 'Tengo muchas ganas de ver la pelicula', '2024-06-30 10:00:00');
+
